29cd7d09b5adee7138c6bd8458c0700dbbacb0a2
qian wan <qiansan@gmail.com>
2019-12-28 11:19:20 -0500

    base

--- src/components/button/base.tsx
+++ src/components/button/base.tsx
@@ -2,5 +2,5 @@ import Button from "react-bootstrap/Button";
 import React from "react";
 
 export default function bxButton(props) {
-  return <Button>{props.children}</Button>;
+  return <div>test</div>;
 }
